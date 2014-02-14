5bd9a2e4c16e7bfd81fd815363f4d717e2ecea65
Titiloss <Titiloss@Titiloss-PC>
2014-02-14 16:37:08 +0100

    c'est la 1ere version

---
+++ resto_Tunisie1/src/resto_tunisie1/Resto1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resto_tunisie1;
+
+/**
+ *
+ * @author Titiloss
+ */
+public class Resto1 {
+    private int id;
+    
+}
---
+++ resto_Tunisie1/src/resto_tunisie1/Resto_Tunisie1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resto_tunisie1;
+
+/**
+ *
+ * @author Titiloss
+ */
+public class Resto_Tunisie1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
